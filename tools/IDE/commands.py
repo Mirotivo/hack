b95fca84d3901c8de039351ba3e30d35660a7344
Amr <Amr.Mostafa@live.com>
2024-08-01 22:46:30 +1000

    refined the software suite

--- tools/IDE/commands.py
+++ tools/IDE/commands.py
@@ -1,11 +1,25 @@
 # commands.py
 from abc import ABC, abstractmethod
-from factory import ToolFactory
+from compiler.jack_translator import JackTranslator
+from vm_translator.vm_translator import VMTranslator
+from assembler.asm_translator import AsmTranslator
 
 class Command(ABC):
     @abstractmethod
     def execute(self, input_data):
         pass
+
+class ToolFactory:
+    @staticmethod
+    def create_tool(tool_type, input_file):
+        if tool_type == "compile":
+            return JackTranslator(input_file)
+        elif tool_type == "translate":
+            return VMTranslator(input_file)
+        elif tool_type == "assemble":
+            return AsmTranslator(input_file)
+        else:
+            raise ValueError("Unknown tool type")
 
 class CompileCommand(Command):
     def execute(self, input_data):
